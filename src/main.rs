72b3d99aa06fb7de75b804c1bdedab5eff6ce5f7
Yusuke Shimizu <44394399+rabhareit@users.noreply.github.com>
2021-02-05 01:46:14 +0900

    :sparkles: Add options

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,98 @@ use std::env;
 use std::process;
 use whoami;
 use serde::{Deserialize, Serialize};
+use reqwest::{Client, StatusCode};
+
+#[macro_use]
+extern crate clap;
+use clap::Arg;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AttachmentField {
+    title: String,
+    value: String,
+    short: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+struct Attachment {
+    color: String,
+    title: String,
+    mrkdwn_in: Vec<String>,
+    fields: Vec<AttachmentField>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+struct WebhookRequest {
+    username: String,
+    text: String,
+    attachments: Vec<Attachment>
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[derive(Serialize, Deserialize, Debug)]
-    struct AttachmentField {
-        title: String,
-        value: String,
-        short: bool,
-    }
+    let cmd = app_from_crate!()
+        .arg(Arg::from_usage("<COMMAND> -c --command <COMMAND> 'Previous command name'"))
+        .arg(Arg::from_usage("<STATUS> -s --status <STATUS> 'Success(0) or fail(else)'"))
+        .arg(Arg::from_usage("<EXECUTED> -a --executed-at <EXECUTED> 'Executed at'"))
+        .arg(Arg::from_usage("<ELAPSED> -e --elapsed <ELAPSED> 'Elapsed time'"))
+        .arg(Arg::with_name("message").help("Message send with status"));
 
-    #[derive(Serialize, Deserialize, Debug)]
-    #[serde(tag = "type")]
-    struct Attachment {
-        color: String,
-        title: String,
-        mrkdwn_in: Vec<String>,
-        fields: Vec<AttachmentField>,
-    }
+    let matches = cmd.get_matches();
 
-    #[derive(Serialize, Deserialize, Debug)]
-    #[serde(tag = "type")]
-    struct WebhookRequest {
-        username: String,
-        text: String,
-        attachments: Vec<Attachment>
-    }
+    let prev_cmd = match matches.value_of("COMMAND") {
+        Some(val) => val.to_string(),
+        None => {
+            println!("Missing option `command`");
+            process::exit(1);
+        }
+    };
 
+    let notif_status = match matches.value_of("STATUS") {
+        Some(val) => val.to_string(),
+        None => {
+            println!("Missing option `status`");
+            process::exit(1);
+        }
+    };
 
-    let args: Vec<String> = env::args().collect();
-    let prev_command = &args[1].to_string();
-    let notif_status = &args[2].to_string();
+    let exec_at = match matches.value_of("EXECUTED") {
+        Some(val) => val.to_string(),
+        None => {
+            println!("Missing option `executed`");
+            process::exit(1);
+        }
+    };
+
+    let elapsed = match matches.value_of("ELAPSED") {
+        Some(val) => {
+            let seconds = val.parse::<i32>().unwrap();
+            match seconds > 60 {
+                true => {
+                    let minutes = seconds / 60;
+                    match minutes > 60 {
+                        true => format!("{}h {}min {}sec", minutes/60, minutes%60, seconds%60),
+                        false => format!("{}min {}sec", minutes, seconds%60)
+                    }
+                },
+                false => format!("{}sec", seconds)
+            }
+       },
+        None => {
+            println!("Missing option `elapsed`");
+            process::exit(1);
+        }
+    };
+
+    let cstm_txt = match matches.value_of("message") {
+        Some(val) => val.to_string(),
+        None => "".to_string()
+    };
+
     let notif_title = if notif_status == "0" {"Command succeeded :ok_woman:"} else {"Command failed :no_good:"};
     let notif_color = if notif_status == "0" {"good"} else {"danger"};
-
-    let exec_at = &args[3].to_string();
-    let elapsed = &args[4].to_string();
 
     let current_dir = match env::current_dir() {
         Ok(val) => val,
@@ -52,26 +108,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let webhook_url = match env::var(webhook_key) {
         Ok(val) => val,
-        Err(err) => {
-            println!("{}: {}", err, webhook_key);
+        Err(_) => {
+            println!("Cannot read Slack webhook URL");
             process::exit(1);
         },
     };
 
     let chairman = match env::var(username_key) {
-        Ok(val) => val,
-        Err(err) => {
-            println!("{}: {}", err, username_key);
-            process::exit(1);
+        Ok(val) => format!("<@{}>", val),
+        Err(_) => {
+            println!("Connot read Slack notify username. Send without @ mention");
+            "".to_string()
         },
     };
     
-    let command_field = AttachmentField {title: "Command".to_string(), value: prev_command.to_string(), short: false};
-    let directory_filed = AttachmentField {title: "Directory".to_string(), value: current_dir.display().to_string(), short: false};
+    let command_field = AttachmentField {title: "Command".to_string(), value: format!("`{}`", prev_cmd), short: false};
+    let directory_filed = AttachmentField {title: "Directory".to_string(), value: format!("`{}`", current_dir.display()), short: false};
     let hostname_field = AttachmentField {title: "Hostname".to_string(), value: whoami::hostname(), short: true};
     let user_field = AttachmentField {title: "User".to_string(), value: whoami::username(), short: true};
-    let exec_field = AttachmentField {title: "executed at".to_string(), value: exec_at.to_string(), short: true};
-    let elapsed_field = AttachmentField {title: "Elapsed time".to_string(), value: elapsed.to_string(), short: true};
+    let exec_field = AttachmentField {title: "executed at".to_string(), value: exec_at, short: true};
+    let elapsed_field = AttachmentField {title: "Elapsed time".to_string(), value: elapsed, short: true};
 
     let attachment = Attachment {
         color: notif_color.to_string(), 
@@ -88,25 +144,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let webhook_req = WebhookRequest {
-        username: "Command result".to_string(),
-        text: "<@".to_string() + &chairman + ">",
+        username: "hicaq".to_string(),
+        text: format!("{} \n {}", chairman, cstm_txt),
         attachments: vec!(attachment)
     };
 
-    let client = reqwest::Client::new();
+    let client = Client::new();
     let res = client.post(&webhook_url)
         .header(reqwest::header::CONTENT_TYPE, "application/json")
         .json(&webhook_req)
         .send()
-        .await;
+        .await?;
 
-    match res {
-        Ok(val) => println!("{:#?}", val),
-        Err(err) => {
-            println!("{}", err);
-            process::exit(1)
+    match res.status() {
+        StatusCode::OK => {
+            println!("Success de gozaru!");
+            Ok(())
+        }
+        s => {
+            println!("Recieved response status: {:?}, katajikenai...", s);
+            process::exit(1); 
         }
     }
-
-    Ok(())
 }
