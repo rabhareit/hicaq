0c388054b3ce084a819017ca857fd5583f410b1b
Yusuke Shimizu <44394399+rabhareit@users.noreply.github.com>
2021-02-05 01:52:02 +0900

    :truck: Add zsh hook file

--- hicaq_hook.sh
+++ hicaq_hook.sh
@@ -8,8 +8,7 @@ function notify_preexec {
 function notify_precmd {
   notif_status=$?
   if [ -n "${CMD_NOTIFY_SLACK_WEBHOOK_URL+x}" ] && [ -n "${CMD_NOTIFY_SLACK_USER_NAME+x}" ] && [ $TTYIDLE -gt ${SLACK_NOTIF_THRESHOLD:-180} ] && [ $notif_status -ne 128 ] && [ $notif_status -ne 129 ] && [ $notif_status -ne 130 ] &    & [ $notif_status -ne 146 ]; then
-    secretary $notif_prev_command $notif_status $notif_prev_exec_at `($TTYIDLE / 60) min ($TTYIDLE % 60) sec`
-
+    hicaq -c $notif_prev_command -s $notif_status -a $notif_prev_exec_at -e $TTYIDLE
   fi
 }
 
